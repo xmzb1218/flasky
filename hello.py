911d3d9b933635bce9df5d880b326fe3d54820cd
xmzb1218 <xmzb1218@users.noreply.github.com>
2015-09-20 22:35:24 +0800

    使用Flask-Script

--- hello.py
+++ hello.py
@@ -1,16 +1,31 @@
 from flask import Flask
 from flask import request
+from flask import make_response
+from flask import redirect
+from flask import abort
+
+from flask.ext.script import Manager
 
 app = Flask(__name__)
 
+manager = Manager(app)
+
 @app.route('/')
 def index():
-	user_agent = request.headers.get('User-Agent')
-	return '<p>You browser is %s!</p>' % user_agent
+#	user_agent = request.headers.get('User-Agent')
+#	return '<p>You browser is %s!</p>' % user_agent
+#	response = make_response('<h1>This document carries a cookie!<h1>')
+#	response.set_cookie('answer', '42')
+#	return response
+	return redirect('http://www.example.com')
 	
-@app.route('/user/<name>')
-def user(name):
-	return '<h1>Hello, %s!</h1>' % name
+@app.route('/user/<id>')
+def get_user(id):
+	user = load_user(id)
+	if not user:
+		abort(404)
+	return '<h1>Hello, %s!</h1>' % user.name
 	
 if __name__ == '__main__':
-	app.run(debug = True)
+#	app.run(debug = True)
+	manager.run()
