7e29360945e5c6184cbf267b3b2d47974bb1a5e1
xmzb1218 <xmzb1218@users.noreply.github.com>
2015-09-20 21:36:49 +0800

    访问request请求上下文查看浏览器类型

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello World!</h1>'
+	user_agent = request.headers.get('User-Agent')
+	return '<p>You browser is %s!</p>' % user_agent
 	
 @app.route('/user/<name>')
 def user(name):
