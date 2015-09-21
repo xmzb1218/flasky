acbd9ce73ebef690204ab7afabcd5e17fd5c255d
xmzb1218 <xmzb1218@users.noreply.github.com>
2015-09-22 01:28:41 +0800

    使用Jinja2渲染模板

--- hello.py
+++ hello.py
@@ -1,30 +1,19 @@
-from flask import Flask
-from flask import request
-from flask import make_response
-from flask import redirect
-from flask import abort
-
+from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-#	user_agent = request.headers.get('User-Agent')
-#	return '<p>You browser is %s!</p>' % user_agent
-#	response = make_response('<h1>This document carries a cookie!<h1>')
-#	response.set_cookie('answer', '42')
-#	return response
-	return redirect('http://www.example.com')
+	return render_template('index.html')
 	
-@app.route('/user/<id>')
-def get_user(id):
-	user = load_user(id)
-	if not user:
-		abort(404)
-	return '<h1>Hello, %s!</h1>' % user.name
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name = name)
 	
 if __name__ == '__main__':
 #	app.run(debug = True)
