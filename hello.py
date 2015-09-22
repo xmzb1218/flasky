00f9d047baaebc9cc4c9fe2c47dc54b9b6a4fe20
xmzb1218 <xmzb1218@users.noreply.github.com>
2015-09-22 12:25:08 +0800

    表单请求重定向和用户会话

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -20,12 +20,11 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	name = None
 	form = NameForm()
 	if form.validate_on_submit():
-		name = form.name.data
-		form.name.data = ''
-	return render_template('index.html', form = form, name = name)
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form = form, name = session.get('name'))
 	
 @app.errorhandler(404)
 def page_not_found(e):
