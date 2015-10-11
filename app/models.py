7ba33e5cc6b22e78782b9dc41984e5c502741592
xmzb1218 <xmzb1218@users.noreply.github.com>
2015-10-12 00:50:48 +0800

    使用Flask-Login认证用户

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from . import db
+from . import db, login_manager
+from flask.ext.login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class Role(db.Model):
@@ -10,9 +11,10 @@ class Role(db.Model):
 	def __repr__(self):
 		return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key = True)
+	email = db.Column(db.String(64), unique = True, index = True)
 	username = db.Column(db.String(64), unique = True, index = True)
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 	password_hash = db.Column(db.String(128))
@@ -30,3 +32,7 @@ class User(db.Model):
 	
 	def __repr__(self):
 		return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
