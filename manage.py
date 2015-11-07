0d0056aa3671114233f1e6bb61890c74b6bd30ee
xmzb1218 <xmzb1218@users.noreply.github.com>
2015-11-08 00:22:39 +0800

    用户角色权限

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Permission
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -9,7 +9,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-	return dict(app = app, db = db, User = User, Role = Role)
+	return dict(app = app, db = db, User = User, Role = Role, Permission = Permission)
 manager.add_command("shell", Shell(make_context = make_shell_context))
 manager.add_command('db', MigrateCommand)
 
